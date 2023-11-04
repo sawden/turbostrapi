53342b2fc6d71e1815ace121855939b11cb94638
sawden <sawden@users.noreply.github.com>
2023-11-04 23:54:57 +0100

    Add demo Content-Types

---
+++ apps/backend/src/admin/app.tsx
@@ -0,0 +1,36 @@
+export default {
+  config: {
+    locales: [
+      // "ar",
+      // "fr",
+      // "cs",
+      "de",
+      "en",
+      // "dk",
+      // "es",
+      // "he",
+      // "id",
+      // "it",
+      // "ja",
+      // "ko",
+      // "ms",
+      // "nl",
+      // "no",
+      // "pl",
+      // "pt-BR",
+      // "pt",
+      // "ru",
+      // "sk",
+      // "sv",
+      // "th",
+      // "tr",
+      // "uk",
+      // "vi",
+      // "zh-Hans",
+      // "zh",
+    ],
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};

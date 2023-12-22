f4a77eca5fe4602d5f8f050ff2606ff3c2f3f92f
sawden <sawden@users.noreply.github.com>
2023-12-22 23:31:19 +0100

    Add JSDoc to licalization lib

--- apps/frontend/src/lib/localization/index.ts
+++ apps/frontend/src/lib/localization/index.ts
@@ -14,12 +14,23 @@ export function localizeHref(href: string, locale: string): string {
   return href === "/" ? `/${locale}` : `/${locale}${href}`;
 }
 
+/**
+ * Returns default locale from array of locales.
+ * @param locales - Array of locales
+ * @returns Default locale or undefined
+ */
 export function getDefaultLocaleFormLocales(
   locales: ApiLocale[],
 ): ApiLocale | undefined {
   return locales.find((locale) => locale.isDefault);
 }
 
+/**
+ * Removes locale from given path.
+ * @param path - URL path
+ * @param locale - Locale code to remove
+ * @returns Path without locale
+ */
 export function removeLocaleFromPath(path: string, locale: string): string {
   const segments = path.split("/");
   if (segments[1] === locale) {
@@ -28,6 +39,12 @@ export function removeLocaleFromPath(path: string, locale: string): string {
   return segments.join("/") || "/";
 }
 
+/**
+ * Adds locale to given path.
+ * @param path - URL path
+ * @param locale - Locale code to add
+ * @returns Path with added locale
+ */
 export function addLocaleToPath(path: string, locale: string): string {
   if (path === "" || path === "/") {
     return `/${locale}`;
@@ -35,6 +52,12 @@ export function addLocaleToPath(path: string, locale: string): string {
   return `/${locale}${path.startsWith("/") ? "" : "/"}${path}`;
 }
 
+/**
+ * Checks if a pathname is missing locale.
+ * @param pathname - URL pathname
+ * @param locales - Array of locales
+ * @returns True if pathname is missing locale, false otherwise
+ */
 export function isPathnameMissingLocale(
   pathname: string,
   locales: ApiLocale[],
@@ -46,6 +69,12 @@ export function isPathnameMissingLocale(
   );
 }
 
+/**
+ * Checks if a pathname has a specific locale.
+ * @param pathname - URL pathname
+ * @param localeCode - Locale code to check for
+ * @returns True if pathname has the locale, false otherwise
+ */
 export function isPathnameLocale(
   pathname: string,
   localeCode: string | undefined,

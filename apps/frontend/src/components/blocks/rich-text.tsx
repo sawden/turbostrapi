06bf12cc081ea9dd67888a32962a958388ea26a2
sawden <sawden@users.noreply.github.com>
2023-12-22 09:02:26 +0100

    Update dependencies

--- apps/frontend/src/components/blocks/rich-text.tsx
+++ apps/frontend/src/components/blocks/rich-text.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
-import type { RootNode } from "@strapi/blocks-react-renderer/dist/BlocksRenderer";
 import type { Attribute } from "@turbostrapi/backend";
 import * as React from "react";
 
@@ -14,11 +13,9 @@ export interface RichTextProps extends React.HTMLAttributes<HTMLDivElement> {
 const RichText = React.forwardRef<HTMLDivElement, RichTextProps>(
   ({ section }, ref) => {
     if (section.content) {
-      // TODO: Remove 'as RootNode[]' after Strapi provides more specific types.
-      const content = section.content as RootNode[];
       return (
         <div className="prose text-center dark:prose-invert md:text-left lg:w-full lg:max-w-5xl">
-          <BlocksRenderer content={content} {...ref} />{" "}
+          <BlocksRenderer content={section.content} {...ref} />{" "}
         </div>
       );
     }

62295ba4de2f6540a12cb42b83e9ef3c8f771451
sawden <sawden@users.noreply.github.com>
2023-12-23 04:44:33 +0100

    Fix style issues

--- apps/frontend/src/components/hero.tsx
+++ apps/frontend/src/components/hero.tsx
@@ -31,7 +31,7 @@ const Hero = React.forwardRef<HTMLDivElement, HeroProps>(
     return (
       <div
         className={cx(
-          "relative z-0 flex flex-col items-center justify-between gap-8 pb-16 pt-12 md:pb-24 md:pt-16 lg:pb-32 lg:pt-20",
+          "relative -z-10 flex flex-col items-center justify-between gap-8 pb-16 pt-12 md:pb-24 md:pt-16 lg:pb-32 lg:pt-20",
           className,
         )}
         ref={ref}
@@ -64,11 +64,10 @@ const Hero = React.forwardRef<HTMLDivElement, HeroProps>(
             className="relative z-50 h-[120px] w-[120px]"
           >
             <div
-              className={`absolute inset-0 flex justify-center transition-opacity duration-1000 ${
-                rotatingVariant === "turborepo"
-                  ? "visible opacity-100"
-                  : "invisible opacity-0"
-              }`}
+              className={`absolute inset-0 flex justify-center transition-opacity duration-1000 ${rotatingVariant === "turborepo"
+                ? "visible opacity-100"
+                : "invisible opacity-0"
+                }`}
             >
               <Image
                 alt="Turborepo logo"
@@ -79,11 +78,10 @@ const Hero = React.forwardRef<HTMLDivElement, HeroProps>(
               />
             </div>
             <div
-              className={`absolute inset-0 flex justify-center transition-opacity duration-1000 ${
-                rotatingVariant === "strapi"
-                  ? "visible opacity-100"
-                  : "invisible opacity-0"
-              }`}
+              className={`absolute inset-0 flex justify-center transition-opacity duration-1000 ${rotatingVariant === "strapi"
+                ? "visible opacity-100"
+                : "invisible opacity-0"
+                }`}
             >
               <Image
                 alt="Strapi logo"

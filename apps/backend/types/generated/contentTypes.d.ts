fd8b65ff12f2fbf8ef9dba3b6179dd460c475ff3
sawden <36924392+sawden@users.noreply.github.com>
2024-03-20 08:56:47 +0100

    Update all dependencies to latest

--- apps/backend/types/generated/contentTypes.d.ts
+++ apps/backend/types/generated/contentTypes.d.ts
@@ -512,6 +512,12 @@ export interface PluginContentReleasesRelease extends Schema.CollectionType {
   attributes: {
     name: Attribute.String & Attribute.Required;
     releasedAt: Attribute.DateTime;
+    scheduledAt: Attribute.DateTime;
+    timezone: Attribute.String;
+    status: Attribute.Enumeration<
+      ["ready", "blocked", "failed", "done", "empty"]
+    > &
+      Attribute.Required;
     actions: Attribute.Relation<
       "plugin::content-releases.release",
       "oneToMany",
@@ -566,6 +572,7 @@ export interface PluginContentReleasesReleaseAction
       "manyToOne",
       "plugin::content-releases.release"
     >;
+    isEntryValid: Attribute.Boolean;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
